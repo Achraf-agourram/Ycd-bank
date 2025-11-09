2ba5336a63c1e8f1ef3f455e61c2b0712527a5c7
hadaz27 <aziz.haddad003@gmail.com>
2025-11-09 14:40:09 +0100

    add : starting login and signup confirmation with js

---
+++ scripts/auth.js
@@ -0,0 +1,7 @@
+const signup_form = document.getElementById("signup_form")
+
+signup_form.addEventListener("submit", () => {
+    event.preventDefault()
+    const signup_password = document.getElementById("signup_password")
+    const signup_confirm_password = document.getElementById("singup_confirm_password")
+})
