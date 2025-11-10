529adbf19a304233836c42683481fab0850a1523
hadaz27 <aziz.haddad003@gmail.com>
2025-11-10 14:47:52 +0100

    add : icon info

--- scripts/auth.js
+++ scripts/auth.js
@@ -6,6 +6,10 @@ const login_page = document.getElementById("login_page")
 const register_now_button = document.getElementById("register_now_button");
 const go_to_login_button = document.getElementById("go_to_login_button");
 
+function hideshow() {
+    signup_page.classList.add("hidden");
+    login_page.classList.remove("hidden");
+}
 // Clic sur "Register Now"
 register_now_button.addEventListener("click", () => {
     login_page.classList.add("hidden");
@@ -14,8 +18,7 @@ register_now_button.addEventListener("click", () => {
 
 // Clic sur "Login"
 go_to_login_button.addEventListener("click", () => {
-    signup_page.classList.add("hidden");
-    login_page.classList.remove("hidden");
+    hideshow()
 })
 
 signup_form.addEventListener("submit", (event) => {
@@ -32,8 +35,51 @@ signup_form.addEventListener("submit", (event) => {
     const first_name = signup_first_name.value
     const last_name = signup_last_name.value
 
-    if (password === "" || password_confirm === "" || first_name === "" || last_name === ""){
-        message_erreur.textContent = "Veuillez remplir tous les champs pour vous inscrire"
+    let pwd_regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]$)/
+    // regex verifications
+    if (password === "" || password_confirm === "" || first_name === "" || last_name === "") {
+        message_erreur.style.color = "red"
+        message_erreur.textContent = "All fields are required"
         return
     }
+
+    if (password !== password_confirm) {
+        message_erreur.style.color = "red"
+        message_erreur.textContent = "Passwords doesn't match"
+        return
+    }
+    if (password.length < 8) {
+        message_erreur.style.color = "red"
+        message_erreur.textContent = "Password must have at least 8 caracters"
+        return
+    }
+
+    if (pwd_regex.test(password)) {
+        console.log("Password Is Valid")
+        return
+    }
+    else {
+        message_erreur.style.color = "red"
+        message_erreur.textContent = "Check info, your Password doesn't match password requirements"
+        return
+    }
+
+    // Stocker ce nouvel utilisateur dans le local storage
+
+    let users = JSON.parse(localStorage.getItem("users")) || []
+
+    const new_user = {
+        prenom: first_name,
+        nom: last_name,
+        mot_de_passe: password
+    }
+    users.push(new_user)
+
+    localStorage.setItem("users", JSON.stringify(users))
+    message_erreur.style.color = "green"
+    message_erreur.textContent = "Sign up success ! You will be redirected to Login Page in 3"
+
+    setTimeout(hideshow, 3000)
+    console.dir(users)
+    signup_form.reset()
 })
