e7ed9fe6138d4a0066f90ad457fae19ecb1e6875
Achraf-podcast <achrafagourram6@gmail.com>
2025-11-10 16:25:46 +0100

    working on showing the first 4 transactions

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const transactions = [
-    {id: 1, date: "2025-10-26", type: "", amount: 100, amountType: true},
-    {id: 2, date: "2025-10-26", type: "", amount: 90, amountType: true},
-    {id: 3, date: "2025-10-26", type: "", amount: 800, amountType: true},
-    {id: 4, date: "2025-10-26", type: "", amount: 35, amountType: true}
+    {id: 1, date: "2025-10-26", type: "Payment", amount: 100, amountType: true},
+    {id: 2, date: "2025-10-26", type: "Transfer", amount: 90, amountType: true},
+    {id: 3, date: "2025-10-26", type: "Transfer", amount: 800, amountType: true},
+    {id: 4, date: "2025-10-26", type: "Recharge", amount: 35, amountType: true}
 ]
+
+console.log(transactions);
